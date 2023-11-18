4fb14d85266e90db38314e904aed004f3112b6f2
anav5704 <anavkki@gmail.com>
2023-11-18 22:24:15 +1200

    fixed deployment issue

--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -1,16 +1,19 @@
-import React, { forwardRef } from "react"
+import { forwardRef } from "react"
 import { cn } from "@/lib/utils"
 
-export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
+interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement>{}
 
 const Button = forwardRef<HTMLButtonElement, ButtonProps>(({
     className, children, disabled, type="button", ...props
 }, ref) => {
     return (
-        <button ref={ref} {...props} className={cn("w-auto bg-black border-transparent px-5 py-3 disabled:cursor-not-allowed disabled:opacity-50 text-white font-semibold hover:opacity-75 transiton", className)}>
+        <button type={type} disabled={disabled} ref={ref} {...props} className={cn("w-auto bg-black border-transparent px-5 py-3 disabled:cursor-not-allowed disabled:opacity-50 text-white font-semibold hover:opacity-75 transiton", className)}>
             {children}
         </button>
     )
 })
+
+// Dont know why this is here but it solved my deployment issue so I guess its staying
+Button.displayName = 'Button'
 
 export default Button
